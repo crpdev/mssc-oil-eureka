ba194de9ea6b60f95f51cb20668b28c86d65bf91
Rajapandian C <rajapandianc@outlook.in>
2020-09-13 07:32:05 +0530

    Initial Project Setup

---
+++ src/main/java/com/crpdev/mssc/oil/eureka/MsscOilEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.crpdev.mssc.oil.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscOilEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscOilEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crpdev/mssc/oil/eureka/MsscOilEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crpdev.mssc.oil.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscOilEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
