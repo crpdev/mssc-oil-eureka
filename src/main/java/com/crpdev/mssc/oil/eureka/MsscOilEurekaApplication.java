7430447ee63334534a90678aea81bc09b7b77e92
Rajapandian C <rajapandianc@outlook.in>
2020-09-14 06:31:07 +0530

    Added Spring Cloud Gateway, Eureka Server and Client Configuration

--- src/main/java/com/crpdev/mssc/oil/eureka/MsscOilEurekaApplication.java
+++ src/main/java/com/crpdev/mssc/oil/eureka/MsscOilEurekaApplication.java
@@ -2,7 +2,9 @@ package com.crpdev.mssc.oil.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MsscOilEurekaApplication {
 
